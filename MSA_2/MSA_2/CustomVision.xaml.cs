5d827f8390c6ca7521607e3ed56a361e08dbf536
bobhanhan <bobhanhan@gmail.com>
2017-07-09 20:15:13 +1200

    Add project and folders

---
+++ MSA_2/MSA_2/CustomVision.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MSA_2 {
+    public partial class CustomVision : ContentView {
+        public CustomVision() {
+            InitializeComponent();
+        }
+    }
+}
